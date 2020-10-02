f7840d00c3c4f3cbe3ff083e1a9f47261775218e
jschneider-r7 <51677548+jschneider-r7@users.noreply.github.com>
2020-10-01 20:09:03 -0400

    Create buckets.user.js

---
+++ buckets.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         Buckets
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description
+// @author
+// @match        https://razorci.osdc.lax.rapid7.com/job/r7-cloud-app-*/*/console*
+// @grant        none
+// @updateURL    
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const bucketPadding = document.createElement("div");
+    bucketPadding.style.cssText = 'height:300px;width:100%';
+    document.getElementById("page-body").appendChild(bucketPadding);
+
+    const bucketDisplay = document.createElement("div");
+    bucketDisplay.style.cssText = 'display:flex;z-index:9999;position:fixed;bottom:75px;right:0px;left:0px;background-color:#dedede;min-height:100px;padding:15px';
+    document.getElementById("page-body").appendChild(bucketDisplay);
+
+    const loading = document.createElement("div");
+    loading.innerHTML = '<img src="/static/908bb441/images/spinner.gif" alt="">';
+    loading.style.cssText = 'margin: auto 5px';
+    bucketDisplay.appendChild(loading);
+
+    const bucketText = document.createElement("div");
+    bucketDisplay.appendChild(bucketText);
+
+    const buckets = [];
+
+    const poll = setInterval(() => {
+        const bucketMatches = document.getElementsByClassName("console-output")[0].textContent.match(/[a-z]+-[a-z]+-[0-9]+:cdn.s3.bucket:.+\/ea-staging\/[0-9]+\//g);
+
+        bucketMatches && bucketMatches.forEach(bucket => {
+            if(!buckets.includes(bucket) && !bucket.includes("<br>")) {
+                buckets.push(bucket)
+                bucketText.innerHTML += (buckets.length > 1 ? "<br>" : "") + bucket;
+            }
+        });
+
+        if(document.body.textContent.indexOf("Finished: SUCCESS") !== -1) {
+            console.log("don");
+            bucketText.innerHTML = buckets.join("<br />")
+            loading.style.cssText += 'visibility:hidden;';
+            clearInterval(poll);
+        }
+    }, 1000);
+})();
